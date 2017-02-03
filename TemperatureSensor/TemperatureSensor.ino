4af9d71d8e4f76b0e09c99204dd17af3483e0356
o-s-e <ose@recommind.com>
2017-02-03 13:29:49 +0000

    Add project files.

---
+++ TemperatureSensor/TemperatureSensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TemperatureSensor.ino
+ Created:	2/3/2017 1:29:46 PM
+ Author:	ose
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
